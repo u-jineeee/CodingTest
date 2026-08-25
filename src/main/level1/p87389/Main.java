ce2b49f35b4dd8c79f7d2532d547d523dff13afe
ujineeee <yujin11006@naver.com>
2026-08-26 00:34:55 +0900

    나머지가 1이 되는 수 찾기

---
+++ src/main/level1/p87389/Main.java
@@ -0,0 +1,15 @@
+package main.level1.p87389;
+
+public class Main {
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+		int result = sol.solution(10);
+		System.out.println(result);
+	}
+}
+class Solution {
+	public int solution(int n) {
+		int answer = 0;
+		return answer;
+	}
+}
